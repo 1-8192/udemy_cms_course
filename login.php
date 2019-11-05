2c2985fc0c592aef82221da80f0b7629eb98cf59
1-8192 <allegranzia@gmail.com>
2019-11-05 10:40:21 -0500

    created db connection with pdo for login page

---
+++ login.php
@@ -0,0 +1,46 @@
+<?php
+    if(isset($_POST['submit'])) {
+
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        $pdo = new PDO('mysql:host=localhost;port=8889;dbname=loginapp', 'test', 'testpass');
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        if ($pdo) {
+            echo "connection success";
+        } else {
+            die("database connection fail");
+        };
+
+        if ($username && $password) {
+
+        } else {
+            echo "fields cannot be blank";
+        };
+    };
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <meta charset="UTF-8">
+    <title>Login Page</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <body>
+        <div class="container">
+            <div class="col-xs-6">
+                <form action="login.php" method="POST">
+                    <div class="form-group">
+                        <label for="username">Username</label>
+                        <input type="text" name="username" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="passwprd">Password</label>
+                        <input type="password" name="password" class="form-control">
+                    </div>
+                    <input class="btn-primary" type="submit" name="submit" value="Submit">
+                </form>
+            </div>
+        </div>
+    </body>
+</html>
