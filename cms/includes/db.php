ac0883bfcd68adca17dfc8d072128fb90de226d6
1-8192 <allegranzia@gmail.com>
2019-11-06 15:34:49 -0500

    created db connection

---
+++ cms/includes/db.php
@@ -0,0 +1,9 @@
+<?php 
+    //establishing PDO connnection to server
+    $pdo = new PDO('mysql:host=localhost;port=8889;dbname=cms', 'root', 'root');
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    if ($pdo) {
+        echo "connection success";
+    } 
+?>
