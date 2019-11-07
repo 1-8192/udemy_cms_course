d37b4e0638d685ca51fa18157373d87294349ab4
1-8192 <allegranzia@gmail.com>
2019-11-07 16:35:54 -0500

    added categories to sidebar and moved widget well to includes folder

--- cms/includes/sidebar.php
+++ cms/includes/sidebar.php
@@ -18,31 +18,29 @@
 
                 <!-- Blog Categories Well -->
                 <div class="well">
+                <?php 
+                        //grabbing categories from db
+                        $query = "SELECT * FROM categories";
+                        $stmt = $pdo->query($query);
+                        $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                    ?>
                     <h4>Blog Categories</h4>
                     <div class="row">
                         <div class="col-lg-6">
                             <ul class="list-unstyled">
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
+                            <?php 
+                            //looping through category names
+                             if (count($rows) > 0) {
+                                foreach($rows as $row) {
+                                    echo('<li><a href="#">'."$row[cat_title]".'</a></li>');
+                                }
+                        }
+                            ?>
                             </ul>
                         </div>
                         <!-- /.col-lg-6 -->
                         <div class="col-lg-6">
                             <ul class="list-unstyled">
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
                             </ul>
                         </div>
                         <!-- /.col-lg-6 -->
@@ -51,10 +49,7 @@
                 </div>
 
                 <!-- Side Widget Well -->
-                <div class="well">
-                    <h4>Side Widget Well</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
-                </div>
+                <?php include_once "widget.php" ?>
 
             </div>
 
