576256179a83e9a0cfa82b2e1bef604f37b8bdba
1-8192 <allegranzia@gmail.com>
2019-11-14 10:17:30 -0500

    index page images are now links

--- cms/index.php
+++ cms/index.php
@@ -45,7 +45,7 @@
                 </p>
                 <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date ?></p>
                 <hr>
-                <img class="img-responsive" src="images/<?php echo $post_image ?>" alt="">
+                <a href="post.php?p_id=<?php echo $post_id ?>" alt="post-header-image"><img class="img-responsive" src="images/<?php echo $post_image ?>" alt=""></a>
                 <hr>
                 <p><?php echo $post_body ?></p>
                 <a class="btn btn-primary" href="post.php?p_id=<?php echo $post_id ?>">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
