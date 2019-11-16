b91141c550b2d813ae86baee2d19479da2ca944a
1-8192 <allegranzia@gmail.com>
2019-11-16 15:19:53 -0500

    bulk action publish and draft options work

--- cms/admin/includes/view_all_posts.php
+++ cms/admin/includes/view_all_posts.php
@@ -1,60 +1,86 @@
 
 <div class="col-xs-6">
-                            <div class="col-xs-6">
-                            <?php 
-                                if (isset($_SESSION['success'])) {
-                                    $message = $_SESSION['success'];
-                                    echo '<h3 style="color:green">'."$message".'</h3>';
-                                }
+    <div class="col-xs-6">
+    <?php 
+        if (isset($_SESSION['success'])) {
+            $message = $_SESSION['success'];
+            echo '<h3 style="color:green">'."$message".'</h3>';
+        }
+    ?>
+    <?php
+        if (isset($_POST['checkBoxArray'])) {
+            foreach($_POST['checkBoxArray'] as $checkBoxValueId) {
+                $bulk_options = $_POST['bulk_options'];
 
-                                if (isset($_POST['checkBoxArray'])) {
-                                    foreach($_POST['checkBoxArray'] as $checkBoxValue) {
-                                        echo "<h1>$checkBoxValue</h1>";
-                                        $bulk_options = $_POST['bulk_options'];
-                                    }
-                                }
-                            ?>
-                            <form action="" method="POST">
-                                <div id="bulkOptionsContainer" clas="col-xs-4">
-                                    <select class="form-control" name="bulk_options">
-                                        <option value="">Select Bulk Option</option>
-                                        <option value="">Publish</option>
-                                        <option value="">Draft</option>
-                                        <option value="">Delete</option>
-                                    </select>
-                                </div>
-                                <div class="col-xs-4">
-                                    <input type="submit" name="submit" class="btn btn-success" value="Apply">
-                                    <a class="btn btn-primary" href="posts.php?source=add_post">Add New Post</a>
-                                </div>
-                            </form>
-                                <table class="table table-bordered table-hover">
-                                    <thead>
-                                        <tr>
-                                            <th><input type="checkbox" id="selectAllBoxes"></th>
-                                            <th>ID</th>
-                                            <th>Author</th>
-                                            <th>Title</th>
-                                            <th>Category</th>
-                                            <th>Status</th>
-                                            <th>Image</th>
-                                            <th>Tags</th>
-                                            <th>Comments</th>
-                                            <th>Date</th>
-                                            <th>Delete</th>
-                                            <th>Edit</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <?php 
-                                        //grabbing posts from db and inserting into table
-                                         fetch_posts();
-                                        ?>
+                switch($bulk_options) {
+                    case 'published':
+                        $query = "UPDATE posts SET post_status = :pstat WHERE post_id = :pid";
+                        $stmt = $pdo->prepare($query);
+                        $stmt->execute(array(
+                            ':pstat' => $bulk_options,
+                            ':pid' => $checkBoxValueId
+                        ));
+                    break;
 
-                                        <?php 
-                                            // deleting post logic
-                                            delete_post();
-                                        ?>
-                                    </tbody>    
-                                </table>
-                            </div>
+                    case 'draft':
+                        $query = "UPDATE posts SET post_status = :pstat WHERE post_id = :pid";
+                        $stmt = $pdo->prepare($query);
+                        $stmt->execute(array(
+                            ':pstat' => $bulk_options,
+                            ':pid' => $checkBoxValueId
+                        ));
+                    break;
+
+                    case 'delete':
+                    break;
+
+                    default:
+                    break;
+                }
+            }
+        }
+    ?>
+    <form action="./posts.php" method="POST">
+        <div id="bulkOptionsContainer" clas="col-xs-4">
+            <select class="form-control" name="bulk_options">
+                <option value="">Select Bulk Option</option>
+                <option value="published">Publish</option>
+                <option value="draft">Draft</option>
+                <option value="delete">Delete</option>
+            </select>
+        </div>
+        <div class="col-xs-4">
+            <input type="submit" value="Apply" class="btn btn-success">
+            <a class="btn btn-primary" href="posts.php?source=add_post">Add New Post</a>
+        </div>
+        <table class="table table-bordered table-hover">
+            <thead>
+                <tr>
+                    <th><input type="checkbox" id="selectAllBoxes"></th>
+                    <th>ID</th>
+                    <th>Author</th>
+                    <th>Title</th>
+                    <th>Category</th>
+                    <th>Status</th>
+                    <th>Image</th>
+                    <th>Tags</th>
+                    <th>Comments</th>
+                    <th>Date</th>
+                    <th>Delete</th>
+                    <th>Edit</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php 
+                //grabbing posts from db and inserting into table
+                    fetch_posts();
+                ?>
+
+                <?php 
+                    // deleting post logic
+                    delete_post();
+                ?>
+            </tbody>    
+        </table>
+        </form>
+</div>
