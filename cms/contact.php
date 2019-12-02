ee4985a56391bc8cb1f6da051ff27ff56a08b3b9
1-8192 <allegranzia@gmail.com>
2019-12-02 16:59:53 -0500

    added contact page

---
+++ cms/contact.php
@@ -0,0 +1,80 @@
+<?php  
+    include "includes/db.php"; 
+    include_once "includes/util_user_functions.php";
+?>
+ <?php  include "includes/header.php"; ?>
+
+
+    <!-- Navigation -->
+    
+    <?php  include "includes/navigation.php"; ?>
+    
+ 
+    <!-- Page Content -->
+    <div class="container">
+<?php 
+ if (isset($_SESSION['error'])) {
+    $message = $_SESSION['error'];
+    echo '<p class="text-center" style="color:red">'."$message".'</p>';
+    unset($_SESSION['error']);
+ }
+
+ if (isset($_POST['submit'])) {
+     if (!empty($_POST['user_name']) && !empty($_POST['user_password']) && !empty($_POST['user_email'])) {
+        $user_name = $_POST['user_name'];
+        $password = $_POST['user_password'];
+        $user_email = $_POST['user_email'];
+        $password = encryptPassword($password);
+
+        $query = "INSERT INTO users (user_name, user_email, user_password, user_role) VALUES (:unm, :em, :pass, :role)";
+        $stmt = $pdo->prepare($query);
+        $stmt->execute(array(
+            ':unm' => $user_name,
+            ':em' => $user_email,
+            ':pass' => $password,
+            ':role' => 'subscriber'
+        ));
+        $_SESSION['success'] = "User succesfully registered";
+        header("Location: index.php");
+
+    } else {
+        $_SESSION['error'] = "Cannot accept empty fields";
+        header("Location: registration.php");
+    }
+}
+?>
+<section id="login">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-6 col-xs-offset-3">
+                <div class="form-wrap">
+                <h1>Register</h1>
+                    <form role="form" action="registration.php" method="post" id="login-form" autocomplete="off">
+                         <div class="form-group">
+                            <label for="user_email" class="sr-only">Email</label>
+                            <input type="email" name="user_email" id="email" class="form-control" placeholder="somebody@example.com">
+                        </div>
+                        <div class="form-group">
+                            <label for="user_name" class="sr-only">Subject</label>
+                            <input type="text" name="subject" id="subject" class="form-control" placeholder="Topic">
+                        </div>
+                         <div class="form-group">
+                            <label for="body" class="sr-only">Message</label>
+                            <textarea name="body" class="form-control" placeholder="I need help..." cols="30" rows="10"></textarea>
+                        </div>
+                
+                        <input type="submit" name="submit" id="btn-login" class="btn btn-custom btn-lg btn-block" value="Register">
+                    </form>
+                 
+                </div>
+            </div> <!-- /.col-xs-12 -->
+        </div> <!-- /.row -->
+    </div> <!-- /.container -->
+</section>
+
+
+        <hr>
+
+
+
+<?php include "includes/footer.php";?>
