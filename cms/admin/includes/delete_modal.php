111e35095cc4afd31dfa854f697a1eebc208f84f
1-8192 <allegranzia@gmail.com>
2019-12-05 08:50:09 -0500

    modal displaying correctly on screen not tied to actual delete

--- cms/admin/includes/delete_modal.php
+++ cms/admin/includes/delete_modal.php
@@ -1,17 +1,18 @@
-<div class="modal" tabindex="-1" role="dialog">
+<button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button>
+<div id="myModal" class="modal fade" tabindex="-1" role="dialog">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title">Modal title</h5>
+        <h5 class="modal-title">Danger!</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
+          <!-- <span aria-hidden="true">&times;</span> -->
         </button>
       </div>
       <div class="modal-body">
-        <p>Modal body text goes here.</p>
+        <p>Are your sure you want to delete the post?</p>
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-primary">Save changes</button>
+        <button type="button" class="btn btn-danger modal-delete-link">Delete</button>
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
       </div>
     </div>
